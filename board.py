ffaf3c40f04b691f0f39a869752ac87036d754eb
pjpure <pure254326@gmail.com>
2022-09-15 18:42:37 +0700

    add pygame & spaghetti

--- board.py
+++ board.py
@@ -1,12 +1,15 @@
+import pygame
+
+
 class BoardManager:
 
-# a = player
-# @ = player ที่ทับบน goal
-# □ = block ที่ไม่อยู่ตรง goal
-# ■ = block ที่อยู่ตรง goal
-# . = goal-
+    # a = player
+    # @ = player ที่ทับบน goal
+    # □ = block ที่ไม่อยู่ตรง goal
+    # ■ = block ที่อยู่ตรง goal
+    # . = goal-
 
-    def __init__(self,board):
+    def __init__(self, board):
         board_ = board.split('\n')[1:-1]
 
         for b in range(len(board_)):
@@ -28,11 +31,26 @@ class BoardManager:
     def getBoardList(self):
         return self.board_lst
 
-    def updateBoard(self,new_board):
+    def updateBoard(self, new_board):
         self.board_lst = new_board
 
     def playerPosition(self):
-       for i in range(len(self.board_lst)):
-        for j in range(len(self.board_lst[i])):
-            if self.board_lst[i][j] == 'a' or self.board_lst[i][j] == '@':
-                return i,j
+        for i in range(len(self.board_lst)):
+            for j in range(len(self.board_lst[i])):
+                if self.board_lst[i][j] == 'a' or self.board_lst[i][j] == '@':
+                    return i, j
+
+    def isGameOver(self):
+        for i in range(len(self.board_lst)):
+            for j in range(len(self.board_lst[i])):
+                if self.board_lst[i][j] == '□':
+                    return False
+        return True
+
+    def genNewBoard(self, board):
+        board_ = board.split('\n')[1:-1]
+
+        for b in range(len(board_)):
+            board_[b] = list(board_[b])
+
+        self.board_lst = board_
--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from board import BoardManager
 from action import Action
 import textwrap
 import os
-
+import pygame
 # a = player
 # @ = player ที่ทับบน goal
 # □ = block ที่ไม่อยู่ตรง goal
 # ■ = block ที่อยู่ตรง goal
 # . = goal
 
-#8x8
-data = ("""
+# 8x8
+data = [
+    """
 ########
 #      #
 ##a   ##
@@ -21,31 +22,76 @@ data = ("""
 #      #
 #      #
 ########
-""")
+""",
+    """
+########
+#      #
+##  a ##
+##.□□  #
+##  #  #
+#      #
+#   .  #
+########
+"""
+]
 
-sokoban = BoardManager(data)
+
+def drawBoard():
+    board = sokoban.getBoardList()
+    for i in range(0, len(board)):
+        for c in range(0, len(board[i])):
+            screen.blit(images[board[i][c]], (c*box_size, i*box_size))
+    pygame.display.update()
+
+
+level = 0
+sokoban = BoardManager(data[level])
 act = Action(sokoban)
 
 
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+pygame.display.set_caption("Sokoban")
+wall = pygame.image.load('assets/images/wall.png')
+box = pygame.image.load('assets/images/box.png')
+box_on_target = pygame.image.load('assets/images/box_on_target.png')
+player = pygame.image.load('assets/images/player.png')
+space = pygame.image.load('assets/images/space.png')
+target = pygame.image.load('assets/images/target.png')
 
+images = {'#': wall, ' ': space, '□': box,
+          '.': target, 'a': player, '■': box_on_target, '@': player}
+box_size = wall.get_width()
+
+drawBoard()
 while True:
-    print(sokoban)
-    inp = input()
-    
-    if inp == "w":
-        act.up()
-    
-    if inp == "s":
-        act.down()
-
-    if inp == "a":
-        act.left()
-
-    if inp == "d":
-        act.right()
-
-    if inp == "x":
-        break
-    os.system('cls')
-
-    #act
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                act.left()
+            if event.key == pygame.K_RIGHT:
+                act.right()
+            if event.key == pygame.K_UP:
+                act.up()
+            if event.key == pygame.K_DOWN:
+                act.down()
+            drawBoard()
+            if sokoban.isGameOver():
+                level += 1
+                level = level % len(data)
+                print("You win!")
+                font = pygame.font.Font('freesansbold.ttf', 32)
+                text = font.render(
+                    'You win!', True, (255, 255, 255), (0, 0, 0))
+                textRect = text.get_rect()
+                textRect.center = (300, 200)
+                screen.blit(text, textRect)
+                pygame.display.update()
+                pygame.time.wait(3000)
+                # clear screen
+                screen.fill((0, 0, 0))
+                sokoban.genNewBoard(data[level])
+                drawBoard()
